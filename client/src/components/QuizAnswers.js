ca3a2dd701a6d2204dbf2815cb3f871b03efbfb7
Clau <clau.rjimenez@gmail.com>
2022-01-25 11:27:57 +0000

    Colour when right or wrong

--- client/src/components/QuizAnswers.js
+++ client/src/components/QuizAnswers.js
@@ -1,35 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import Button from 'react-bootstrap/Button'
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
 
-const QuizAnswers = ({answer}) => {
-    
-    const [colour,setColour] = useState("primary")
+const QuizAnswers = ({ answer }) => {
+    const [colour, setColour] = useState("primary");
 
+    const handleClick = (event) => {
+        console.log(event.target.value);
 
-    const handleClick =(event)=>{
-        
-        if (event.target.value ===true){
-            setColour('success')
-            console.log(colour)
-            
-        }else if(event.target.value === false){
-            setColour('danger')
+        if (event.target.value === "true") {
+            setColour("success");
+        } else if (event.target.value === "false") {
+            setColour("danger");
         }
-    }
+    };
 
-
-
-   
-    return( 
-    <div className='answers-container'>
-        
-        <Button  variant={colour} onClick= {handleClick} value={answer.isCorrect} >{answer.text}</Button>
-        
-          
-       
-        
-    </div>
-  );
+    return (
+        <div className="answers-container">
+            <Button
+                variant={colour}
+                onClick={handleClick}
+                value={answer.isCorrect}
+            >
+                {answer.text}
+            </Button>
+        </div>
+    );
 };
 
 export default QuizAnswers;
